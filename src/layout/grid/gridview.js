6985b0d0a07338bf8db1e76102208642a3edcd96
mm2288 <47225267+mm2288@users.noreply.github.com>
2020-04-15 12:30:45 -0400

    Adding a navbar

--- src/layout/grid/gridview.js
+++ src/layout/grid/gridview.js
@@ -6,11 +6,40 @@ const COLUMN_NAMES = ['todo', 'in-progress', 'review', 'done'];
 
 class GridView extends React.Component {
 
+    state = {
+      breakpoint: 'computer',
+      browserWidth: 0
+    }
+
+    componentDidMount() {
+      window.addEventListener('resize', this.resizeScreen);
+      this.resizeScreen();
+      window.addEventListener('change', this.changeColumn);
+    }
+
     constructor(props) {
         super(props);
 
         this.onPrevClick = this.onPrevClick.bind(this);
         this.onNextClick = this.onNextClick.bind(this);
+
+    }
+
+    resizeScreen = () => {
+      const browserWidth = window.screen.width;
+      let breakpoint = 'computer';
+
+      if (browserWidth <= 768) {
+        breakpoint = 'mobile';
+      }
+
+      this.setState({ breakpoint, browserWidth });
+    }
+
+    changeColumn = () => {
+      let column = document.getElementById("columns").value;
+      console.log(column);
+      this.setState({ column });
     }
 
     findTask(taskId, columnName) {
@@ -64,7 +93,20 @@ class GridView extends React.Component {
         const doneCards = this.props.tasks.done
             .map(post => this.renderCardColumn(post, '< Request Re-Review'));
 
+        if (this.state.breakpoint === 'computer'){
         return (
+            <div className="container">
+
+            <div className="mobile-nav">
+            <h3>Select a column to view:</h3>
+            <select id="columns">
+              <option value="todo">To Do</option>
+              <option value="in-progress">In Progress</option>
+              <option value="review">Review</option>
+              <option value="done">Done</option>
+            </select>
+            </div>
+
             <div className="row">
                 <div className="col to-do">
                     <h2>
@@ -103,9 +145,132 @@ class GridView extends React.Component {
                     </div>
                 </div>
             </div>
-        );
+
+
+            </div>
+          );
+          }
+
+          else if (this.state.breakpoint === 'mobile') {
+              if (this.state.column === 'todo'){
+                return (
+                  <div className="container">
+
+                  <div className="mobile-nav">
+                  <h3>Select a column to view:</h3>
+                  <select id="columns">
+                    <option value="todo">To Do</option>
+                    <option value="in-progress">In Progress</option>
+                    <option value="review">Review</option>
+                    <option value="done">Done</option>
+                  </select>
+                  </div>
+                  <div className="row">
+                      <div className="col to-do">
+                          <h2>
+                              To Do
+                          </h2>
+
+                          <div className="">
+                              { todoCards }
+                          </div>
+                      </div>
+                  </div>
+                </div>
+              );
+              }
+
+
+              else if (this.state.column === 'in-progress'){
+                  return (
+                    <div className="container">
+
+                    <div className="mobile-nav">
+                    <h3>Select a column to view:</h3>
+                    <select id="columns">
+                      <option value="todo">To Do</option>
+                      <option value="in-progress">In Progress</option>
+                      <option value="review">Review</option>
+                      <option value="done">Done</option>
+                    </select>
+                    </div>
+
+                    <div className="col in-progress">
+                        <h2>
+                            In Progress
+                        </h2>
+
+                        <div className="">
+                            { inProgressCards }
+                        </div>
+                    </div>
+
+                  </div>
+                );
+                }
+
+
+
+                else if (this.state.column === 'review'){
+                    return (
+                      <div className="container">
+
+                        <div className="mobile-nav">
+                        <h3>Select a column to view:</h3>
+                        <select id="columns">
+                          <option value="todo">To Do</option>
+                          <option value="in-progress">In Progress</option>
+                          <option value="review">Review</option>
+                          <option value="done">Done</option>
+                        </select>
+                        </div>
+
+                        <div className="col review">
+                            <h2>
+                                Review
+                            </h2>
+
+                            <div className="">
+                                { reviewCards }
+                            </div>
+                        </div>
+
+                    </div>
+                  );
+                  }
+
+                  else if (this.state.column === 'done'){
+                      return (
+                        <div className="container">
+
+                        <div className="mobile-nav">
+                        <h3>Select a column to view:</h3>
+                        <select id="columns">
+                          <option value="todo">To Do</option>
+                          <option value="in-progress">In Progress</option>
+                          <option value="review">Review</option>
+                          <option value="done">Done</option>
+                        </select>
+                        </div>
+
+                        <div className="col done">
+                            <h2>
+                                Done
+                            </h2>
+
+                            <div className="">
+                                { doneCards }
+                            </div>
+                        </div>
+
+                      </div>
+                    );
+                    }
+
+
+              }
+          }
     }
 
-}
 
 export default GridView;
